1f2f638eeb510504287cc8dc9a12f835c9eec652
Yoann Grange <yoann.grange@gmail.com>
2020-12-16 10:18:55 +0100

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,28 @@
+const footerTemplate = document.createElement('template');
+footerTemplate.innerHTML = `
+  <footer class="text-muted">
+      <div class="container">
+
+        <br>
+        <hr>
+        <br>
+
+        <p><small>ProductPeople.io est un projet porté par <a href="https://fr.linkedin.com/in/yoanngrange" target="_blank">Yoann Grange</a>, hébergé sur <a href="https://pages.github.com/" target="_blank">Github Pages</a>, qui utilise <a href="http://v4-alpha.getbootstrap.com/" target="_blank">Bootstrap 4</a> et <a href="https://fonts.google.com/specimen/Copse/" target="_blank"> Copse</a>. Les points de vue exprimés sur ce site ne sont que des points de vue.</small></p>
+      </div>
+    </footer>
+
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'closed' });
+    shadowRoot.appendChild(footerTemplate.content);
+  }
+}
+
+customElements.define('footer-component', Footer);
