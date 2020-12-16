04d2c6d65fc6624cd9e224ac1b67e44f46c6dd79
Yoann Grange <yoann.grange@gmail.com>
2020-12-16 15:09:34 +0100

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -12,7 +12,7 @@ class Footer extends HTMLElement {
         <hr>
         <br>
 
-        <p><small>ProductPeople.io est un projet porté par <a href="https://fr.linkedin.com/in/yoanngrange" target="_blank">Yoann Grange</a>, hébergé sur <a href="https://pages.github.com/" target="_blank">Github Pages</a>, qui utilise <a href="http://v4-alpha.getbootstrap.com/" target="_blank">Bootstrap 4</a> et <a href="https://fonts.google.com/specimen/Copse/" target="_blank"> Copse</a>. Les points de vue exprimés sur ce site ne sont que des points de vue.</small></p>
+        <p><small>ProductPeople.io est un projet porté par <a href="https://www.linkedin.com/in/yoanngrange/" target="_blank">Yoann Grange</a>. Les points de vue exprimés sur ce site ne sont que des points de vue. Ces derniers datent de décembre 2016.</small></p>
       </div>
     </footer>
 
