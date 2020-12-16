8d6a07d02fcda46574cdd193a13cdc7e900dcc4f
Yoann Grange <yoann.grange@gmail.com>
2020-12-16 10:36:34 +0100

    Create head.js

---
+++ js/head.js
@@ -0,0 +1,21 @@
+class Head extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Yoann Grange">
+    <link rel="icon" href="favicon.ico">
+    <link href="https://fonts.googleapis.com/css?family=Copse" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/footer.js" type="text/javascript" defer></script>
+    `;
+  }
+}
+
+customElements.define('head-component', Head);
