d89224672c0e092229529fdcd5647d9fc10fa8ad
Yoann Grange <yoann.grange@gmail.com>
2020-12-16 15:05:51 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -8,11 +8,11 @@ class Header extends HTMLElement {
     <section class="jumbotron text-xs-center">
       <div class="container">
         <a href="index.html">
-          <img src="img/logo-product-people.png">
+          <img width="200" src="img/logo-product-people.png">
         </a>
         <br>
         <br>
-        <p class="lead text-muted">25 interviews vidéo de Product Owners, Product Managers, Head of Product... afin de faire connaître ces métiers.</p>
+        <p class="lead text-muted">25 interviews de Product Owners, Product Managers & Heads of Product</p>
         </form>
       </div>
     </section>
